846dcc4d8faeb746405431a1087096aefa2e0330
sreeram <sreeramjayavaram3@gmail.com>
2025-10-10 14:17:50 +0530

    same issue

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 import userModel from '../models/userModel.js';
+import axios from 'axios';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
